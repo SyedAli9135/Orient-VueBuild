2bc5606080ada4952047555022e3e21ec1064873
SyedAli9135 <84285222+SyedAli9135@users.noreply.github.com>
2021-08-11 10:59:56 +0500

    Add files via upload

---
+++ src/components/ProductPage1.vue
@@ -0,0 +1,138 @@
+<template>
+  <div>
+    <Navbar />
+    <Header2 />
+    <div>
+      <div className="productpage1">
+        <div v-for="product in products" :key="product.id">
+          <div className="productpage1-column">
+            <img :src="product.image" style="width: 100%;" />
+            <strong>{{ product.title }}</strong>
+            <p>
+              <strong>{{ product.text }}</strong>
+            </p>
+            <p>{{ product.price }}</p>
+            <div className="productpage1-buttons">
+              <button>EN M/ US M</button>
+              <router-link to="productcheckout"><button>ADD</button></router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+<script>
+import Navbar from './Navbar.vue'
+import Header2 from './Header2.vue'
+import Footer from './Footer.vue'
+export default {
+  name: 'ProductPage1',
+  components: {
+    Navbar,
+    Header2,
+    Footer,
+  },
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          image: '/images/Rectangle55.png',
+          title: 'NEW',
+          text: 'UNSTICHED 3PC PREMIUM ',
+          price: 'PKR 4,990.00',
+        },
+        {
+          id: 2,
+          image: '/images/Rectangle56.png',
+          title: 'NEW',
+          text: 'UNSTICHED 3PC PREMIUM ',
+          price: 'PKR 4,990.00',
+        },
+        {
+          id: 3,
+          image: '/images/Rectangle63.png',
+          title: 'NEW',
+          text: 'UNSTICHED 3PC PREMIUM ',
+          price: 'PKR 4,990.00',
+        },
+        {
+          id: 4,
+          image: '/images/Rectangle64.png',
+          title: 'NEW',
+          text: 'UNSTICHED 3PC PREMIUM ',
+          price: 'PKR 4,990.00',
+        },
+        {
+          id: 5,
+          image: '/images/Rectangle63.png',
+          title: 'NEW',
+          text: 'UNSTICHED 3PC PREMIUM ',
+          price: 'PKR 4,990.00',
+        },
+        {
+          id: 6,
+          image: '/images/Rectangle64.png',
+          title: 'NEW',
+          text: 'UNSTICHED 3PC PREMIUM ',
+          price: 'PKR 4,990.00',
+        },
+        {
+          id: 7,
+          image: '/images/Rectangle63.png',
+          title: 'NEW',
+          text: 'UNSTICHED 3PC PREMIUM ',
+          price: 'PKR 4,990.00',
+        },
+        {
+          id: 8,
+          image: '/images/Rectangle64.png',
+          title: 'NEW',
+          text: 'UNSTICHED 3PC PREMIUM ',
+          price: 'PKR 4,990.00',
+        },
+      ],
+    }
+  },
+}
+</script>
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+.productpage1-column {
+  float: left;
+  width: 25%;
+  padding: 10px;
+}
+.productpage1-column strong {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.productpage1-column p {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.productpage1-buttons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+/* Clearfix (clear floats) */
+.productpage1::after {
+  content: '';
+  clear: both;
+  display: table;
+}
+
+/* Responsive layout - makes the three columns stack on top of each other instead of next to each other */
+@media screen and (max-width: 768px) {
+  .productpage1-column {
+    width: 100%;
+  }
+}
+</style>
---
+++ src/components/ProductPage2Modified.vue
@@ -0,0 +1,190 @@
+<script>
+import Navbar from './Navbar.vue'
+import Header1 from './Header1.vue'
+import Footer from './Footer.vue'
+export default {
+  name: 'ProductPage2Modified',
+  components: {
+    Navbar,
+    Header1,
+    Footer,
+  },
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          image: '/images/Rectangle55.png',
+          title: 'NEW',
+          text: 'UNSTICHED 3PC PREMIUM ',
+          price: 'PKR 4,990.00',
+          size: 'small',
+        },
+        {
+          id: 2,
+          image: '/images/Rectangle56.png',
+          title: 'NEW',
+          text: 'UNSTICHED 3PC PREMIUM ',
+          price: 'PKR 4,990.00',
+          size: 'small',
+        },
+        {
+          id: 3,
+          image: '/images/Rectangle63.png',
+          title: 'NEW',
+          text: 'UNSTICHED 3PC PREMIUM ',
+          price: 'PKR 4,990.00',
+          size: 'large',
+        },
+        {
+          id: 4,
+          image: '/images/Rectangle64.png',
+          title: 'NEW',
+          text: 'UNSTICHED 3PC PREMIUM ',
+          price: 'PKR 4,990.00',
+          size: 'small',
+        },
+        {
+          id: 5,
+          image: '/images/Rectangle63.png',
+          title: 'NEW',
+          text: 'UNSTICHED 3PC PREMIUM ',
+          price: 'PKR 4,990.00',
+          size: 'small',
+        },
+        {
+          id: 6,
+          image: '/images/Rectangle64.png',
+          title: 'NEW',
+          text: 'UNSTICHED 3PC PREMIUM ',
+          price: 'PKR 4,990.00',
+          size: 'large',
+        },
+        {
+          id: 7,
+          image: '/images/Rectangle63.png',
+          title: 'NEW',
+          text: 'UNSTICHED 3PC PREMIUM ',
+          price: 'PKR 4,990.00',
+          size: 'small',
+        },
+        {
+          id: 8,
+          image: '/images/Rectangle64.png',
+          title: 'NEW',
+          text: 'UNSTICHED 3PC PREMIUM ',
+          price: 'PKR 4,990.00',
+          size: 'small',
+        },
+      ],
+    }
+  },
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Header1 />
+        <div id="productpage2-row-wrapper" class="productpage2-row">
+          <div className="productpage2-myclass">
+            {this.products && this.products.length
+              ? this.products.map((item) =>
+                  this.products.indexOf(item) === 2 ||
+                  (this.products.indexOf(item) > 2 &&
+                    (this.products.indexOf(item) + 1) % 3 === 0) ? (
+                    <div className="productpage2-myclass2">
+                      <img class="card img-top" src={item.image} alt="" />
+                      <div class="card-body">
+                        <h5 class="card-title">
+                          <strong>{item.title}</strong>
+                        </h5>
+                        <p class="card-text">
+                          <strong>{item.text}</strong>
+                        </p>
+                        <p class="card-text">
+                          <small class="text-muted">{item.price}</small>
+                        </p>
+                      </div>
+                    </div>
+                  ) : (
+                    <div id="card-child" class="card border-0">
+                      <img
+                        id="img-width"
+                        class="card img-top"
+                        src={item.image}
+                        alt=""
+                      />
+                      <div id="productpage1-content" class="card-body">
+                        <h5 class="card-title">
+                          <strong>{item.title}</strong>
+                        </h5>
+                        <p class="card-text">
+                          <strong>{item.text}</strong>
+                        </p>
+                        <p class="card-text">
+                          <small class="text-muted">{item.price}</small>
+                        </p>
+                      </div>
+                    </div>
+                  ),
+                )
+              : null}
+          </div>
+        </div>
+        <Footer />
+      </div>
+    )
+  },
+}
+</script>
+<style scoped>
+#productpage2-row-wrapper {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin-right: 0px;
+  margin-left: 0px;
+}
+
+.productpage2-myclass {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  width: 100%;
+  margin-left: 10%;
+}
+
+.productpage2-myclass2 {
+  display: flex;
+  flex-direction: column;
+  margin-right: 30%;
+  margin-left: 20%;
+  width: 30%;
+}
+
+#card-child {
+  margin-right: 200px;
+
+  width: 30%;
+}
+#productpage1-content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+@media only screen and (max-width: 768px) {
+  .productpage2-myclass2 {
+    margin-right: 0;
+    margin-left: 0;
+    width: 90%;
+  }
+  #card-child {
+    margin-right: 0px;
+    width: 90%;
+  }
+  #productpage2-row-wrapper {
+    margin-right: 0px;
+  }
+}
+</style>
